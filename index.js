741ac8689b10f6ca7ed273e941c7691e73364508
chinmoy <chinmoybora27@gmail.com>
2024-09-15 19:40:37 +0530

    how to import json

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 // import { sum } from "./calculate/sum.js";
 // const obj=require("./sum")
 const { sum, multipication } = require("./calculate");
+const data=require("./data.json")
 console.log("first commit")
 let a=10;
 let b=9;
 sum(a,b)
 multipication(a,b)
+console.log(data)
 // console.log(sum(a,b))
 // console.log(multipication(a,b))
 // sum(a,b)
